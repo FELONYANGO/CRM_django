b27461e4a7a905da42caffd7868a6b33f47fcd88
FELONYANGO <felixonyango951@gmail.com>
2023-01-31 02:27:32 +0300

    created the table models

---
+++ Leads/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+
+class Lead(models.Model):
+
+
+    # SOURECE_CHOICES = (('youtube', 'youtube'),
+    
+    # ('twiter', 'twiter'),
+    # ('googleplus', 'googleplus'),
+    # )
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    Age = models.IntegerField(default=0)
+    agent=models.ForeignKey("Agent",on_delete=models.CASCADE)
+    # Phoned=models.BooleanField(default=False)
+    # sources = models.char_field(choices=SOUREC_SOURCE,max_length=100)
+    # profiles = models.ImageField(null=True, blank=True)
+    # Special_files = models.FileField(null=True, blank=True)
+
+class Agent(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
